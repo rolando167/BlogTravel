e57ad7a545b29a1b88cd142f588cf993178ff08a
rolando167 <rolando167@hotmail.com>
2020-06-16 11:04:35 -0500

    Updates

---
+++ precache-manifest.cb1d1d6433566dda5f02fbc72d6574eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6dcd106f86b332955eb1e3922c33574",
+    "url": "/BlogTravel/index.html"
+  },
+  {
+    "revision": "b5870186ea129de75135",
+    "url": "/BlogTravel/static/js/2.0a014ad8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BlogTravel/static/js/2.0a014ad8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02a7d1d00e11c1e4e5e9",
+    "url": "/BlogTravel/static/js/main.a4eb0436.chunk.js"
+  },
+  {
+    "revision": "270e62b9e7203b82c56d",
+    "url": "/BlogTravel/static/js/runtime-main.20ffef47.js"
+  }
+]);
